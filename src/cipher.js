9d2d560af336e8c702429e76df96f5b1c2bf65ed
ivonneconde <ivonneconde91@gmail.com>
2023-07-19 14:51:53 -0600

    Se han hecho los cambios finales en cuanto al botón y otros temas que tenía pendientes y creo que está listo el proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,24 @@
 const cipher = {
-  // ...
-};
+encode: function(mensaje, offset) {
+  let resultado = "";
+  for (let i = 0; i < mensaje.length; i++) {
+    let c = mensaje.charCodeAt(i);
 
+    if (c >= 65 && c <= 90) {
+      resultado += String.fromCharCode(((c - 65 + offset) % 26) + 65);
+    } else {
+      resultado += mensaje.charAt(i);
+    }
+  }
+  return resultado;
+},
+
+decode: function (mensaje, offset) {
+  let resultado = "";
+  offset = (26 - offset) % 26;
+  resultado = cipher.encode(mensaje, offset);
+  return resultado;
+}
+}
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import cipher from './cipher.js';
+document.addEventListener("DOMContentLoaded", function() {
+    iniciar();
+  });
+const encode = cipher.encode;
+const decode =cipher.decode;
+
+function iniciar() {
+  document.getElementById("mensaje").addEventListener(
+    "keyup",
+    function () {
+      this.value = this.value.toUpperCase();
+    },
+    true
+  );
+
+  document.getElementById("encode").addEventListener(
+    "click",
+    function () {
+          let mensaje = document.getElementById("mensaje").value;
+          let offset = parseInt(document.getElementById("offset").value);
+
+          document.getElementById("respuesta").value = cipher.encode(mensaje, offset);
+      },
+    true
+  );
+
+  document.getElementById("decode").addEventListener(
+    "click",
+    function () {
+      let mensaje = document.getElementById("mensaje").value;
+      let offset = parseInt(document.getElementById("offset").value);
+      document.getElementById("respuesta").value = cipher.decode(mensaje, offset);
+    },
+    true
+  );
+}
 
 console.log(cipher);
+
+export default cipher;
+
+iniciar();
+
