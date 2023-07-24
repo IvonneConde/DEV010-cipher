4fc1d18a7d9ca303fb77c9658866ad8dde5894dd
ivonneconde <ivonneconde91@gmail.com>
2023-07-24 12:51:22 -0600

    Updates

---
+++ cipher.js
@@ -0,0 +1,40 @@
+const cipher = {
+  encode: function (offset, mensaje) {
+    if (typeof mensaje !== 'string' || typeof offset !== 'number') {
+      throw new TypeError('La contraseña y el desplazamiento deben ser de tipo string y number respectivamente');
+    }
+
+    let resultado = '';
+    for (let i = 0; i < mensaje.length; i++) {
+      const c = mensaje.charCodeAt(i);  //charCodeAt este MÉTODO se usa para obtener el valor de un caracter de un string que ya existe
+
+      if (c >= 65 && c <= 90) {
+        // Si la letra es mayúscula (código ASCII entre 65 y 90)
+        // Aplicamos el desplazamiento y ajustamos para que vuelva al inicio del alfabeto si se pasa del final
+        const letraCifrada = String.fromCharCode(((c - 65 + offset) % 26) + 65);  //String.fromCharCode este MÉTODO es para crear un string apartir de
+        resultado += letraCifrada;
+      } else if (c >= 97 && c <= 122) {
+        // Si la letra es minúscula (código ASCII entre 97 y 122)
+        // Aplicamos el desplazamiento y ajustamos para que vuelva al inicio del alfabeto si se pasa del final
+        const letraCifrada = String.fromCharCode(((c - 97 + offset) % 26) + 97);
+        resultado += letraCifrada;
+      } else {
+        // Si no es una letra, se agrega directamente al resultado
+        resultado += mensaje.charAt(i);  //Método charAt para obtener el caracter de un string
+      }
+    }
+    return resultado;
+  },
+
+  decode: function (offset, mensaje) {
+    if (typeof mensaje !== 'string' || typeof offset !== 'number') {
+      throw new TypeError('La contraseña y el desplazamiento deben ser de tipo string y number respectivamente');
+    }
+
+    offset = (26 - (offset % 26)) % 26;
+    const resultado = cipher.encode(offset, mensaje);
+    return resultado;
+  },
+};
+
+export default cipher;
---
+++ index.js
@@ -0,0 +1,48 @@
+import cipher from './cipher.js';
+document.addEventListener("DOMContentLoaded", function() {  // Espera a que el DOM esté completamente cargado para iniciar la función 'iniciar()'
+  iniciar();
+});
+
+
+function iniciar() {
+  document.getElementById("mensaje").addEventListener(
+    "keyup",
+    function () {
+      this.value = this.value.toUpperCase();
+    },
+    true
+  );
+
+  // Escucha el evento 'click' en el botón con id 'encode'
+  // Obtiene el mensaje y el offset 
+  // Llama a la función 'encode' del módulo 'cipher' para cifrar el mensaje con el offset
+  // y muestra el resultado en 'respuesta'
+  document.getElementById("encode").addEventListener(
+    "click",    // Argumento
+    function () {    // Argumento
+      const mensaje = document.getElementById("mensaje").value;      // Obtener el mensaje del usuario
+      const offset = parseInt(document.getElementById("offset").value);      // Obtener el desplazamiento (offset) ingresado por el usuario
+      document.getElementById("respuesta").value = cipher.encode(offset, mensaje);
+    },
+    true  // Argumento
+  );
+
+  document.getElementById("decode").addEventListener(
+    "click",
+    function () {
+      const mensaje = document.getElementById("mensaje").value;
+      const offset = parseInt(document.getElementById("offset").value);
+      document.getElementById("respuesta").value = cipher.decode(offset, mensaje);
+    },
+    true
+  );
+
+
+}
+
+// console.log(cipher);
+
+export default cipher;
+
+iniciar();
+
